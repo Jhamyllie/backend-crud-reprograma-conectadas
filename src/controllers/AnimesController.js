3d0736b848d5eec29682fada40f198189e6a184c
Jamile <milli.santana2017@gmail.com>
2023-10-31 20:03:08 -0300

    fix: remove verificação para testar se ainda assim, funciona

--- src/controllers/AnimesController.js
+++ src/controllers/AnimesController.js
@@ -74,7 +74,7 @@ const updateAnime = async (req, res) => {
 		});
 
 		return res.status(200).json({message: "Update completed successfully", updating});
-		
+
 	} catch (error) {
 		return res.status(500).json({message: error.message});
 	}
